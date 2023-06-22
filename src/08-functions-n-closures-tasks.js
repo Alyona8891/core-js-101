ba06a972e6fbdd8b2864667584e2d0e1e970bb7b
unknown <besssta888@mail.ru>
2023-06-23 00:07:05 +0300

    feat: add my solution

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -195,8 +195,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  const arr = [];
+  arr.push(startFrom);
+  return () => {
+    arr.push(arr[arr.length - 1] + 1);
+    return arr[arr.length - 2];
+  };
 }
 
 
